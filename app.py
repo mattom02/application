44b3b12ec381afe92c7993beae475f6c9b69b7e1
mattom02 <129222002+mattom02@users.noreply.github.com>
2023-05-26 14:33:25 +0200

    Implementacja klasy głównej Zaklad

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+import datetime
 
+class Zaklad:
+  def __init__(self, miejsce, stawka, kurs):
+    self.miejsce = miejsce
+    self.stawka = stawka
+    self.kurs = kurs
+    self.data = datetime.datetime.now()
