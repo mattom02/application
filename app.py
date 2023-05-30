d9e073c73c80991781bbb6c0690a82ee83d7b96d
Nyxul <117312305+Nyxul@users.noreply.github.com>
2023-05-30 12:16:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,14 @@ class Bukmacher:
   def oblicz_wypłate(self, stawka, kurs):
     wygrana = stawka * kurs
     return wygrana
+
+  def miejsce_zakladu(self):
+    while True:
+      try: 
+        miejsce = int(input("Wybierz numer miejsca zakladu (1, 2, 3, 4, 5)"))
+        if miejsce < 1:
+          print("Wybierz poprawny numer miejsca!")
+        else:
+          return miejsce
+      except ValueError:
+        print("Podaj poprawny numer miejsca!")
